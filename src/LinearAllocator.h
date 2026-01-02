9f8ff987f8d4b9680d04e98a7c7ef01434f263d4
Nadia Nadeem <nadianadeem365@gmail.com>
2026-01-02 19:42:46 +0000

    Add templated object/array allocation helpers to LinearAllocator
    Introduce New and NewArray templated methods to LinearAllocator for convenient and type-safe allocation and construction of single objects and arrays. These helpers use placement new and support both default and value initialization, reducing boilerplate for allocator users.

--- src/LinearAllocator.h
+++ src/LinearAllocator.h
@@ -3,11 +3,43 @@
 
 #include <cstddef>
 #include <cstdint>
+#include <utility>
+#include <new>
 
 class LinearAllocator {
 public:
     LinearAllocator(size_t size);
     ~LinearAllocator();
+
+    template<typename T>
+    T* New() {
+        void* mem = Allocate(sizeof(T), alignof(T));
+        return new (mem) T();
+    }
+
+    template<typename T, typename... Args>
+    T* New(Args&&... args) {
+        void* mem = Allocate(sizeof(T), alignof(T));
+        return new (mem) T(std::forward<Args>(args)...);
+    }
+
+    template<typename T>
+    T* NewArray(size_t count) {
+        void* mem = Allocate(sizeof(T) * count, alignof(T));
+        T* arr = static_cast<T*>(mem);
+        for (size_t i = 0; i < count; ++i)
+            new (&arr[i]) T();
+        return arr;
+    }
+
+    template<typename T>
+    T* NewArray(size_t count, const T& value) {
+        void* mem = Allocate(sizeof(T) * count, alignof(T));
+        T* arr = static_cast<T*>(mem);
+        for (size_t i = 0; i < count; ++i)
+            new (&arr[i]) T(value);
+        return arr;
+    }
 
     void* Allocate(size_t size, size_t alignment = alignof(std::max_align_t));
     void Reset();
