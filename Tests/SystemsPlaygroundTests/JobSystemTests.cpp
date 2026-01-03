f8200eb48ca40bf2ee1fb5c994a5ae83d0eba26f
Nadia Nadeem <nadianadeem365@gmail.com>
2026-01-03 10:51:47 +0000

    Google Test GitHubAction for Test Automation. Tests for LinearAllocator and JobSystem.

---
+++ Tests/SystemsPlaygroundTests/JobSystemTests.cpp
@@ -0,0 +1,14 @@
+
+#include "../../Engine/src/Core/JobSystem.h"
+
+#include <gtest/gtest.h>
+
+TEST(JobSystem, JobRunsWhenSubmitted) {
+    JobSystem jobs;
+
+    bool ran = false;
+    auto h = jobs.Submit([&] { ran = true; });
+
+    jobs.Wait(h);
+    EXPECT_TRUE(ran);
+}
---
+++ Tests/SystemsPlaygroundTests/LinearAllocatorTests.cpp
@@ -0,0 +1,14 @@
+
+#include "../../Engine/src/Core/LinearAllocator.h"
+
+#include <gtest/gtest.h>
+
+TEST(LinearAllocator, ResetsMemoryCorrectly) {
+    LinearAllocator alloc(128);
+
+    void* p1 = alloc.Allocate(64);
+    alloc.Reset();
+    void* p2 = alloc.Allocate(64);
+
+    EXPECT_EQ(p1, p2);
+}
