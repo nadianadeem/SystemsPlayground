6e688bfb9c208449788a833ad19c38abe2e211f1
Nadia Nadeem <nadianadeem365@gmail.com>
2026-01-04 19:04:35 +0000

    Revert "Add new unit tests for JobSystem and LinearAllocator"
    This reverts commit e104c3435c39c56fa8824d85c9659999bc33f974.

--- Tests/SystemsPlaygroundTests/CoreTests/LinearAllocatorTests.cpp
+++ Tests/SystemsPlaygroundTests/CoreTests/LinearAllocatorTests.cpp
@@ -5,12 +5,6 @@
 
 #include <gtest/gtest.h>
 
-struct TestStruct
-{
-    int a;
-    TestStruct() : a(7) {}
-};
-
 TEST(LinearAllocator, ResetsMemoryCorrectly) {
     LinearAllocator alloc(128);
 
@@ -19,32 +13,4 @@ TEST(LinearAllocator, ResetsMemoryCorrectly) {
     void* p2 = alloc.Allocate(64);
 
     EXPECT_EQ(p1, p2);
-}
-
-TEST(LinearAllocator, AllocatesAlignedMemory) 
-{
-    LinearAllocator alloc(128);
-    void* p1 = alloc.Allocate(16, 16);
-    EXPECT_EQ(reinterpret_cast<uintptr_t>(p1) % 16, 0);
-    void* p2 = alloc.Allocate(32, 32);
-    EXPECT_EQ(reinterpret_cast<uintptr_t>(p2) % 32, 0);
-}
-
-TEST(LinearAllocator, NewOperatorWorks) 
-{
-    LinearAllocator alloc(256);
-
-    TestStruct* obj = alloc.New<TestStruct>();
-    EXPECT_EQ(obj->a, 7);
-}
-
-TEST(LinearAllocator, NewArrayOperatorWorks) 
-{
-    LinearAllocator alloc(512);
-
-    size_t count = 5;
-    TestStruct* arr = alloc.NewArray<TestStruct>(count);
-    for (size_t i = 0; i < count; ++i) {
-        EXPECT_EQ(arr[i].a, 7);
-    }
 }
